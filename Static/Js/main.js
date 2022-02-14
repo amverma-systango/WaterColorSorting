f3b9ae603d78d2b38644153a48b583b7ebbac827
Aman Verma <amanverma03328@gmail.com>
2022-02-15 00:26:48 +0530

    logic for making color bottol dynamically added

--- Static/Js/main.js
+++ Static/Js/main.js
@@ -1,31 +1,51 @@
+// main function ( its a self executing function run when the html is rendered completely )
+
 (function() {
 
-   // Levels
+	// Levels
+	const LEVELS = {
+	  1 : {
+	     1 : {
+	        "totalColors": 1,
+	        "totalSegmentInOneBottle": 2,
+	        "totalBottle": 2,
+	        "colorArr":[ ["R"], ["R"] ]
+	     },
+	     2 : {
+	        "totalColors": 1,
+	        "totalSegmentInOneBottle": 3,
+	        "totalBottle": 3,
+	        "colorArr":[ ["B"], ["B"], ["B"] ]
+	     },
+	     3 : {
+	        "totalColors": 1,
+	        "totalSegmentInOneBottle": 2,
+	        "totalBottle": 2,
+	        "colorArr":[ ["G"], ["G"] ]
+	     },
+	  }
+	};
+	// console.log(LEVELS);
+	// Levels end
 
-   const LEVELS = {
-      1 : {
-         1 : {
-            totalColors: 1,
-            totalSegmentInOneBottle: 2,
-            totalBottle:2,
-            "arr":[ ["R"], ["R"] ]
-         },
-         2 : {
-            totalColors: 1,
-            totalSegmentInOneBottle: 3,
-            totalBottle:3,
-            "arr":[ ["B"], ["B"], ["B"] ]
-         },
-         3 : {
-            totalColors: 1,
-            totalSegmentInOneBottle: 2,
-            totalBottle:2,
-            "arr":[ ["G"], ["G"] ]
-         },
-      }
-   };
 
-   // Levels end
+	// Colors
+	const COLORPALLET= {
+		"B": "blue", 
+		"C": "cyan",
+		"F": "firebrick",
+		"G": "green",
+		"I": "indigo",
+		"M": "moccasin",
+		"O": "orange",
+		"P": "purple",
+		"R": "red",
+		"T": "teal",
+		"V": "violet",
+		"Y": "yellow",
+	};
+	// console.log(COLORPALLET);
+	// Colors end
 
 
 
@@ -37,11 +57,16 @@
    let currentLevel = 1;
    let currentObjectLength = Object.keys(LEVELS[currentLevel]).length;
    let currentComboNumber = getRandomInt(1,currentObjectLength+1);
-   let currentComboArray = LEVELS[currentLevel][currentComboNumber];
+   let currentComboObject = LEVELS[currentLevel][currentComboNumber];
 
+   /*
+   //debugging statements
    console.log(LEVELS[currentLevel]);
    console.log(currentComboNumber);
-   console.log(currentComboArray);
+   console.log(currentComboObject);
+   */
+
+   bottleDrawer(currentComboObject["totalBottle"],currentComboObject["totalSegmentInOneBottle"], currentComboObject["colorArr"]);
 
 })();
 
@@ -55,6 +80,36 @@ function getRandomInt( min = 0 , max = 0) {
 }
 // function end
 
-//console.log(levels);
+
+
+function bottleDrawer( totalBottlesToDraw, totalSegmentInOneBottle, colorArr ){
+	let parentBoxContainingColorBottles = document.getElementById("parentBox");
+
+	parentBoxContainingColorBottlesInnerHtml = "";
+	//alert(parentBox.innerText);
+
+	console.log("in bottleDrawer funciton");
+	console.log(totalBottlesToDraw);
+	console.log(totalSegmentInOneBottle);
+	console.log(colorArr);
+
+	for(let bottleIter=0; bottleIter<totalBottlesToDraw; bottleIter++){
+		let oneColorBottle = `<div id="${bottleIter+1}" class="bottles mx-4 p-1">`;
+
+		for(let segmentIter=0; segmentIter < totalSegmentInOneBottle ; segmentIter++)
+		{
+			oneColorBottle = oneColorBottle +
+			`<div class = "colorSegment my-1 mx-auto">`+
+			`${bottleIter+1}`+
+			`</div>`; // closing the segment div
+		}
+
+		oneColorBottle = oneColorBottle + `</div>`;  // closing the bottle div
+
+		parentBoxContainingColorBottlesInnerHtml = parentBoxContainingColorBottlesInnerHtml + oneColorBottle;
+	}
+
+	parentBoxContainingColorBottles.innerHTML = parentBoxContainingColorBottlesInnerHtml;
+}
 
 

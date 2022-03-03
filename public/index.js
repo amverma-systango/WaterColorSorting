4d6803c90de796a43aa791c99fe82e2ecfe2f074
amverma-systango <amverma@isystango.com>
2022-03-03 19:38:42 +0530

    UI fixes, overflow problem

--- public/index.js
+++ public/index.js
@@ -1,13 +1,32 @@
-if("serviceWorker" in navigator){
-  navigator.serviceWorker.register("./serviceWorker.js").then((response) =>{
-    console.log("SW registered!");
-    console.log(registration);
-  }).catch(error=>{
-    console.log("SW Registration Failed");
-    console.log(error);
-  });
+function determineAppServerKey() {
+  var vapidPublicKey = 'BPza1bfrN-GvYJzeTk5wmY-_1IR5MQSYmr64rkn1vxXIFYrKF60MQ4gSGzKMoK8HEnG5uaEUvMOKmIBXHthCCvk';
+  return urlBase64ToUint8Array(vapidPublicKey);
 }
 
 
+function urlBase64ToUint8Array(base64String) {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
 
-     
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./serviceWorker.js").then((response) => {
+    return response.pushManager.getSubscription().then(function (subscription) {
+      return response.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: determineAppServerKey()
+      })
+    })
+
+  });
+}
